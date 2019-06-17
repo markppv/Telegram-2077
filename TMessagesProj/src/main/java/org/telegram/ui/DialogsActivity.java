0dbb842a3eaf39fe8607970022ea5f2b960531bd
Mark Popov <mark@coldtouch.ru>
2019-06-17 16:47:30 +0300

    Changed app name to «Telegram 2077»

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -850,9 +850,9 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                 actionBar.setTitle(LocaleController.getString("ArchivedChats", R.string.ArchivedChats));
             } else {
                 if (BuildVars.DEBUG_VERSION) {
-                    actionBar.setTitle("Telegram Beta"/*LocaleController.getString("AppNameBeta", R.string.AppNameBeta)*/);
+                    actionBar.setTitle("Telegram 2077 Beta"/*LocaleController.getString("AppNameBeta", R.string.AppNameBeta)*/);
                 } else {
-                    actionBar.setTitle(LocaleController.getString("AppName", R.string.AppName));
+                    actionBar.setTitle("Telegram 2077"/*LocaleController.getString("AppName", R.string.AppName)*/);
                 }
             }
             actionBar.setSupportsHolidayImage(true);
