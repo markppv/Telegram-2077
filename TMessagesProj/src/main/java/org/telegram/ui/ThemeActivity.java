766773b3c88547246693233669b7001e6abca888
Mark Popov <mark@coldtouch.ru>
2019-06-19 20:51:44 +0300

    Enable swipeback when recycler view with themes reached the left edge in ThemeActivity

--- TMessagesProj/src/main/java/org/telegram/ui/ThemeActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ThemeActivity.java
@@ -1522,7 +1522,7 @@ public class ThemeActivity extends BaseFragment implements NotificationCenter.No
                         @Override
                         public boolean onInterceptTouchEvent(MotionEvent e) {
                             if (getParent() != null && getParent().getParent() != null) {
-                                getParent().getParent().requestDisallowInterceptTouchEvent(true);
+                                getParent().getParent().requestDisallowInterceptTouchEvent(canScrollHorizontally(-1));
                             }
                             return super.onInterceptTouchEvent(e);
                         }
