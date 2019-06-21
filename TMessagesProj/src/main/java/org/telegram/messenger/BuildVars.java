02227dfa987c3f95421819f988b12691235f2cd0
Mark Popov <mark@qmans.ru>
2019-06-21 13:03:19 +0300

    Updated maps key, app id, app hash and restore info

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,7 +13,7 @@ import android.content.SharedPreferences;
 
 public class BuildVars {
 
-    public static boolean DEBUG_VERSION = true;
+    public static boolean DEBUG_VERSION = false;
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static boolean LOGS_ENABLED = false;
     public static boolean USE_CLOUD_STRINGS = true;
@@ -21,8 +21,8 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = false;
     public static int BUILD_VERSION = 1608;
     public static String BUILD_VERSION_STRING = "5.7.0";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static int APP_ID = 16632;
+    public static String APP_HASH = "39dc0490efbaf09e87fead936909dfab";
     public static String HOCKEY_APP_HASH = "";
     public static String HOCKEY_APP_HASH_DEBUG = "";
     public static String SMS_HASH = ""; //https://developers.google.com/identity/sms-retriever/overview
