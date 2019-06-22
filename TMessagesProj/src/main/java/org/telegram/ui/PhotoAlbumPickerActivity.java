121ed7b76dbd8dd97b0866da4c5e972f38ae75b2
Mark Popov <mark@qmans.ru>
2019-06-22 14:34:30 +0300

    Changed the menu button in Photo Album Picker

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoAlbumPickerActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoAlbumPickerActivity.java
@@ -32,6 +32,7 @@ import org.telegram.messenger.R;
 import org.telegram.messenger.SendMessagesHelper;
 import org.telegram.ui.ActionBar.ActionBar;
 import org.telegram.ui.ActionBar.ActionBarMenu;
+import org.telegram.ui.ActionBar.ActionBarMenuItem;
 import org.telegram.ui.ActionBar.BaseFragment;
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Cells.PhotoPickerAlbumsCell;
@@ -140,7 +141,9 @@ public class PhotoAlbumPickerActivity extends BaseFragment implements Notificati
         });
 
         ActionBarMenu menu = actionBar.createMenu();
-        menu.addItem(1, R.drawable.ic_ab_other).setContentDescription(LocaleController.getString("AccDescrMoreOptions", R.string.AccDescrMoreOptions));
+        ActionBarMenuItem item = menu.addItem(0, R.drawable.ic_ab_other);
+        item.setContentDescription(LocaleController.getString("AccDescrMoreOptions", R.string.AccDescrMoreOptions));
+        item.addSubItem(1, R.drawable.profile_photos, "Use system gallery");
 
         fragmentView = new FrameLayout(context);
 
